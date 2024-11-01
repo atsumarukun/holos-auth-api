bf3f65a1e67873ae37e8292f0daac182470dfc54
atsumarukun <atys.smz@gmail.com>
2024-11-01 18:41:33 +0000

    create: DBへの接続.

--- internal/app/api/server.go
+++ internal/app/api/server.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"holos-auth-api/internal/pkg/config"
 	"log"
 	"net/http"
 	"os"
@@ -10,9 +11,35 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 func Serve() {
+	file, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	log.SetOutput(file)
+
+	c := &mysql.Config{
+		Addr:      config.MySQLHost + ":" + config.MySQLPort,
+		User:      config.MySQLUser,
+		Passwd:    config.MySQLPassword,
+		DBName:    config.MySQLDatabase,
+		Net:       "tcp",
+		ParseTime: true,
+	}
+	db, err := sqlx.Open("mysql", c.FormatDSN())
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer db.Close()
+	if err := db.Ping(); err != nil {
+		log.Fatalln(err)
+	}
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World")
@@ -22,13 +49,6 @@ func Serve() {
 		Addr:    ":8000",
 		Handler: r,
 	}
-
-	file, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	log.SetOutput(file)
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, os.Interrupt, os.Kill)
 	defer stop()

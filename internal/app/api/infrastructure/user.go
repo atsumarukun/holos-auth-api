08902969e84e84a14fb3fca15257b4d2aab96f52
atsumarukun <atys.smz@gmail.com>
2024-11-04 07:36:59 +0000

    create: Userインフラストラクチャを作成.

---
+++ internal/app/api/infrastructure/user.go
@@ -0,0 +1,61 @@
+package infrastructure
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"holos-auth-api/internal/app/api/domain/entity"
+	"holos-auth-api/internal/app/api/domain/repository"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type userInfrastructure struct {
+	db *sqlx.DB
+}
+
+func NewUserInfrastructure(db *sqlx.DB) repository.UserRepository {
+	return &userInfrastructure{
+		db: db,
+	}
+}
+
+func (ui *userInfrastructure) Create(ctx context.Context, user *entity.User) error {
+	_, err := ui.db.NamedExecContext(
+		ctx,
+		`INSERT INTO users (id, name, password, created_at, updated_at) VALUES (:id, :name, :password, :created_at, :updated_at);`,
+		user,
+	)
+	return err
+}
+
+func (ui *userInfrastructure) Update(ctx context.Context, user *entity.User) error {
+	_, err := ui.db.NamedExecContext(
+		ctx,
+		`UPDATE users SET name = :name, password = :password, updated_at :updated_at WHERE id = :id AND deleted_at IS NULL LIMIT 1;`,
+		user,
+	)
+	return err
+}
+
+func (ui *userInfrastructure) Delete(ctx context.Context, user *entity.User) error {
+	_, err := ui.db.NamedExecContext(
+		ctx,
+		`UPDATE users SET updated_at = :updated_at, deleted_at = NOW(6) WHERE id = :id AND deleted_at IS NULL LIMIT 1;`,
+		user,
+	)
+	return err
+}
+
+func (ui *userInfrastructure) FindOneByName(ctx context.Context, name string) (*entity.User, error) {
+	var user entity.User
+	err := ui.db.QueryRowxContext(
+		ctx,
+		`SELECT id, name, password, created_at, updated_at FROM users WHERE name = ? AND deleted_at IS NULL LIMIT 1;`,
+		name,
+	).StructScan(&user)
+	if err != nil && errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	}
+	return &user, err
+}

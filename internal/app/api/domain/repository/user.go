8dfa69f1df02856218fae787f3f6c63e07ac180e
atsumarukun <atys.smz@gmail.com>
2024-11-06 14:05:07 +0000

    create: Userドメインサービスのユニオンテストコードを作成.

--- internal/app/api/domain/repository/user.go
+++ internal/app/api/domain/repository/user.go
@@ -1,3 +1,4 @@
+//go:generate mockgen -source=$GOFILE -destination=../../../../../test/mock/domain/repository/$GOFILE
 package repository
 
 import (
---
+++ internal/app/api/domain/service/user_test.go
@@ -0,0 +1,63 @@
+package service_test
+
+import (
+	"context"
+	"holos-auth-api/internal/app/api/domain/entity"
+	"holos-auth-api/internal/app/api/domain/service"
+	mock_repository "holos-auth-api/test/mock/domain/repository"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+)
+
+func TestExists(t *testing.T) {
+	tests := []struct {
+		name        string
+		password    string
+		isReturnNil bool
+		expect      bool
+	}{
+		{
+			name:        "exists",
+			password:    "password",
+			isReturnNil: false,
+			expect:      true,
+		},
+		{
+			name:        "not_exists",
+			password:    "password",
+			isReturnNil: true,
+			expect:      false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			user, err := entity.NewUser(tt.name, tt.password, tt.password)
+			if err != nil {
+				t.Error(err.Error())
+			}
+
+			res := user
+			if tt.isReturnNil {
+				res = nil
+			}
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			ctx := context.Background()
+
+			ur := mock_repository.NewMockUserRepository(ctrl)
+			ur.EXPECT().FindOneByName(ctx, tt.name).Return(res, nil)
+
+			us := service.NewUserService(ur)
+			exists, err := us.Exists(ctx, user)
+			if err != nil {
+				t.Error(err.Error())
+			}
+			if exists != tt.expect {
+				t.Errorf("expect %t but got %t", tt.expect, exists)
+			}
+		})
+	}
+}

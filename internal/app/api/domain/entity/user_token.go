61898be239274d7dad4356edb2b8e2ae8826c6c7
atsumarukun <atys.smz@gmail.com>
2024-11-09 15:07:34 +0000

    create: UserTokenエンティティを作成.

---
+++ internal/app/api/domain/entity/user_token.go
@@ -0,0 +1,45 @@
+package entity
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"holos-auth-api/internal/pkg/apierr"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var ErrUserTokenTooLong = apierr.NewApiError(http.StatusInternalServerError, "user token must be 32 characters or less")
+
+type UserToken struct {
+	UserID    uuid.UUID `db:"user_id"`
+	Token     string    `db:"token"`
+	ExpiresAt time.Time `db:"expires_at"`
+}
+
+func NewUserToken(userID uuid.UUID) (*UserToken, apierr.ApiError) {
+	ut := &UserToken{
+		UserID: userID,
+	}
+
+	if err := ut.GenerateToken(); err != nil {
+		return nil, err
+	}
+
+	return ut, nil
+}
+
+func (ut *UserToken) GenerateToken() apierr.ApiError {
+	buf := make([]byte, 24)
+	if _, err := rand.Read(buf); err != nil {
+		return apierr.NewApiError(http.StatusInternalServerError, err.Error())
+	}
+	token := base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(buf)
+	if 32 < len(token) {
+		return ErrUserTokenTooLong
+	}
+	ut.Token = token
+	ut.ExpiresAt = time.Now().Add(time.Hour * 24 * 30)
+	return nil
+}

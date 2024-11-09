993ff56f31fa284cf2f5ccfa02177a6f51e08197
atsumarukun <atys.smz@gmail.com>
2024-11-09 15:34:34 +0000

    create: UserTokenリポジトリを作成.

---
+++ internal/app/api/domain/repository/user_token.go
@@ -0,0 +1,14 @@
+//go:generate mockgen -source=$GOFILE -destination=../../../../../test/mock/domain/repository/$GOFILE
+package repository
+
+import (
+	"context"
+	"holos-auth-api/internal/app/api/domain/entity"
+	"holos-auth-api/internal/pkg/apierr"
+)
+
+type UserTokenRepository interface {
+	Save(context.Context, *entity.UserToken) apierr.ApiError
+	Delete(context.Context, *entity.UserToken) apierr.ApiError
+	FindOneByTokenAndNotExpired(context.Context, string) (*entity.UserToken, apierr.ApiError)
+}

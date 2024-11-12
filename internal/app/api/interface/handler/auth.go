83cd3fb9bcf9da7a08f1c026e67cfa3afdbaca04
atsumarukun <atys.smz@gmail.com>
2024-11-12 13:31:44 +0000

    create: AuthハンドラにGetUserID関数を作成.

--- internal/app/api/interface/handler/auth.go
+++ internal/app/api/interface/handler/auth.go
@@ -13,6 +13,7 @@ import (
 type AuthHandler interface {
 	Signin(*gin.Context)
 	Signout(*gin.Context)
+	GetUserID(*gin.Context)
 }
 
 type authHandler struct {
@@ -58,4 +59,22 @@ func (ah *authHandler) Signout(c *gin.Context) {
 	}
 
 	c.Status(http.StatusNoContent)
+}
+
+func (ah *authHandler) GetUserID(c *gin.Context) {
+	bearerToken := strings.Split(c.Request.Header.Get("Authorization"), " ")
+	if len(bearerToken) != 2 || bearerToken[0] != "Bearer" {
+		c.String(http.StatusUnauthorized, "unauthorized")
+		return
+	}
+
+	ctx := context.Background()
+
+	userID, err := ah.authUsecase.GetUserID(ctx, bearerToken[1])
+	if err != nil {
+		c.String(err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, userID.String())
 }

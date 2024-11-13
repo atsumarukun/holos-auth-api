34df56db09ee908d575250bdfc85c59f0f48bf68
atsumarukun <atys.smz@gmail.com>
2024-11-13 13:59:32 +0000

    create: UserハンドラにUpdateName関数を作成.

--- internal/app/api/interface/handler/user.go
+++ internal/app/api/interface/handler/user.go
@@ -13,6 +13,7 @@ import (
 
 type UserHandler interface {
 	Create(*gin.Context)
+	UpdateName(*gin.Context)
 	UpdatePassword(*gin.Context)
 	Delete(*gin.Context)
 }
@@ -45,8 +46,37 @@ func (uh *userHandler) Create(c *gin.Context) {
 	c.JSON(http.StatusOK, response.NewUserResponse(dto.Name, dto.CreatedAt, dto.UpdatedAt))
 }
 
+func (uh *userHandler) UpdateName(c *gin.Context) {
+	var req request.UpdateUserNameRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	userID, exists := c.Get("userID")
+	if !exists {
+		c.String(http.StatusInternalServerError, "context does not have user id")
+		return
+	}
+
+	id, ok := userID.(uuid.UUID)
+	if !ok {
+		c.String(http.StatusInternalServerError, "Invalid user id type")
+	}
+
+	ctx := context.Background()
+
+	dto, err := uh.userUsecase.UpdateName(ctx, id, req.Name)
+	if err != nil {
+		c.String(err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, response.NewUserResponse(dto.Name, dto.CreatedAt, dto.UpdatedAt))
+}
+
 func (uh *userHandler) UpdatePassword(c *gin.Context) {
-	var req request.UpdateUserRequest
+	var req request.UpdateUserPasswordRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.String(http.StatusBadRequest, err.Error())
 		return

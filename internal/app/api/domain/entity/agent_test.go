41fe66c46a20790e15b494419aaf9f314d3694ef
atsumarukun <atys.smz@gmail.com>
2024-11-17 10:17:13 +0000

    create: Agentエンティティのユニオンテストコードを作成.

---
+++ internal/app/api/domain/entity/agent_test.go
@@ -0,0 +1,86 @@
+package entity_test
+
+import (
+	"holos-auth-api/internal/app/api/domain/entity"
+	"holos-auth-api/internal/pkg/apierr"
+	"strings"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestNewAgent(t *testing.T) {
+	agent, err := entity.NewAgent(uuid.New(), "name")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if agent.ID == uuid.Nil {
+		t.Error("id: expect uuid but got empty")
+	}
+	if agent.UserID == uuid.Nil {
+		t.Error("user_id: expect uuid but got empty")
+	}
+	if agent.Name == "" {
+		t.Error("name: expect string but got empty")
+	}
+	if agent.CreatedAt.IsZero() {
+		t.Error("created_at: expect time but got empty")
+	}
+	if agent.UpdatedAt.IsZero() {
+		t.Error("updated_at: expect time but got empty")
+	}
+	if !agent.CreatedAt.Equal(agent.UpdatedAt) {
+		t.Error("expect created_at and updated_at to be equal")
+	}
+}
+
+func TestAgent_SetName(t *testing.T) {
+	tests := []struct {
+		name   string
+		expect apierr.ApiError
+	}{
+		{
+			name:   "valid_NAME",
+			expect: nil,
+		},
+		{
+			name:   "invalid-NAME",
+			expect: entity.ErrInvalidAgentName,
+		},
+		{
+			name:   "なまえ",
+			expect: entity.ErrInvalidAgentName,
+		},
+		{
+			name:   strings.Repeat("a", 2),
+			expect: entity.ErrAgentNameTooShort,
+		},
+		{
+			name:   strings.Repeat("a", 3),
+			expect: nil,
+		},
+		{
+			name:   strings.Repeat("a", 255),
+			expect: nil,
+		},
+		{
+			name:   strings.Repeat("a", 256),
+			expect: entity.ErrAgentNameTooLong,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(t.Name(), func(t *testing.T) {
+			agent, err := entity.NewAgent(uuid.New(), "name")
+			if err != nil {
+				t.Error(err.Error())
+			}
+			if err := agent.SetName(tt.name); err != tt.expect {
+				if err == nil {
+					t.Error("expect err but got nil")
+				} else {
+					t.Error(err.Error())
+				}
+			}
+		})
+	}
+}

11d6fe04bd6812d962a774981a73e7e4fd7b45a1
atsumarukun <atys.smz@gmail.com>
2024-11-17 14:14:31 +0000

    create: Agentハンドラーのユニオンテストコードを作成.

--- internal/app/api/interface/handler/user_test.go
+++ internal/app/api/interface/handler/user_test.go
@@ -262,7 +262,7 @@ func TestUser_Delete(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			req, err := http.NewRequest("DELETE", "/user/:name", bytes.NewBuffer([]byte(tt.requestJSON)))
+			req, err := http.NewRequest("DELETE", "/user", bytes.NewBuffer([]byte(tt.requestJSON)))
 			if err != nil {
 				t.Error(err.Error())
 			}
